ebca8dde216cbda18478ace965a4fb45dd855b16
Joshua Mack <joshnoodle@gmail.com>
2025-05-31 15:41:18 -0400

    Trying 0.3 delay

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -229,7 +229,7 @@ class CarInterface(CarInterfaceBase):
     ret.autoResumeSng = candidate in (HONDA_BOSCH | {CAR.HONDA_CIVIC})
     ret.minEnableSpeed = -1. if ret.autoResumeSng else 25.51 * CV.MPH_TO_MS
 
-    ret.steerActuatorDelay = 0.1
+    ret.steerActuatorDelay = 0.3
     ret.steerLimitTimer = 0.8
     ret.radarDelay = 0.1
 
